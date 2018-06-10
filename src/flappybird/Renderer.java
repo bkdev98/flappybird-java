6d9e621aaa0f453a9d1229d3d1cdad9ad01cb4b1
Quốc Khánh <bkdev98@gmail.com>
2018-06-10 15:43:36 +0700

    initials

---
+++ src/flappybird/Renderer.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package flappybird;
+
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author notkhanh
+ */
+public class Renderer extends JPanel {
+    
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        
+        FlappyBird.flappyBird.repaint(g);
+    }
+}
